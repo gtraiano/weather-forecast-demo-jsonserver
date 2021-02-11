072fe9f521929210846baa9353130371a25f49a7
gtraiano <gtraiano@gmail.com>
2021-02-11 22:23:54 +0200

    fixed CRUD operations methods

--- backend/forecastDatabase.js
+++ backend/forecastDatabase.js
@@ -1,60 +1,92 @@
-const { MongoClient } = require('mongodb');
+const axios = require('axios');
+const db = require('./database/database');
  
-const dbName = 'weather-forecast';
-const collectionName = 'forecasts';
-const uri = `mongodb://localhost:${process.env.MONGODB_PORT}`;
-let client = null;
-let database = null;
-let collection = null;
-
+const dbName = 'onecall';
+const uri = `http://localhost:${process.env.JSON_SERVER_PORT}/${dbName}`;
 
 const insertCity = async city => {
-	const result = await collection.updateOne({ lat: Number.parseFloat(city.lat), lon: Number.parseFloat(city.lon) }, { $set: { ...city } }, { upsert: true });
-	return result;
+	try {
+		const result = await Promise.resolve(
+			axios.post(`${uri}?lat=${Number.parseFloat(city.lat)}&lon=${Number.parseFloat(city.lon)}`,
+				{ ...city },
+				{ headers: { 'Content-Type': 'application/json' } }
+			)
+		);
+		return result.data;
+	}
+	catch(error) {
+		console.log(`Inserting (lat:${city.lat} lon:${city.lon}) failed`);
+		return null;
+	}
 }
 
 const insertCities = async cities => {
 	const result = await Promise.all( cities.map(async city => insertCity(city)) );
-	return result;
+	return result.data;
 }
 
 const updateCity = async (lat, lon, data) => {
-	const result = await collection.updateOne({ lat: Number.parseFloat(lat), lon: Number.parseFloat(lon) }, { $set: { ...data } });
-	return result;
+	try {
+		const exists = await findCity(lat, lon);
+		const result = await Promise.resolve(
+			axios.put(
+				`${uri}/${exists.id}`,
+				{ ...data },
+				{ headers: { 'Content-Type': 'application/json' } }
+			)
+		);
+		return result.data;
+	}
+	catch(error) {
+		console.log(`lat: ${lat} lon: ${lon} does not exist`);
+		return null;
+	}
+}
+
+const updateCities = async cities => {
+	const result = await Promise.all( cities.map(async city => updateCity(city.lat, city.lon, city)) );
+	return result.data;	
 }
 
 const findCity = async (lat, lon) => {
-	const result = await collection.findOne({ lat: Number.parseFloat(lat), lon: Number.parseFloat(lon) });
-	return result;
+	try {
+		const result = await Promise.resolve(axios.get(`${uri}?lat=${Number.parseFloat(lat)}&lon=${Number.parseFloat(lon)}`));
+		return result.data[0];
+	}
+	catch(error) {
+		console.log(`lat: ${lat} lon: ${lon} does not exist`);
+		return null;
+	}
 }
 
 const allCities = async () => {
-	const result = await collection.find({});
-	return result.toArray();
+	const result = await Promise.resolve(axios.get(uri));
+	return result.data;
 }
 
 const removeCity = async (lat, lon) => {
-	const result = await collection.deleteOne({ lat: Number.parseFloat(lat), lon: Number.parseFloat(lon) });
-	return result;
+	try {
+		const exists = await findCity(lat, lon);
+		const result = await Promise.resolve(axios.delete(`${uri}/${exists.id}`));
+		return result.status;
+	}
+	catch(error) {
+		console.log(`lat: ${lat} lon: ${lon} does not exist`);
+		return 404;
+	}
 }
 
 const clearDatabase = async () => {
-	await collection.deleteMany({});
+	//await collection.deleteMany({});
 }
 
 const connect = async () => {
-	client = await MongoClient.connect(uri, { useUnifiedTopology: true });
-	database = await client.db(dbName);
-	collection = await database.collection(collectionName);
+	await db.checkDb();
+	await Promise.resolve(axios.get(`http://localhost:${process.env.JSON_SERVER_PORT}/ping`));
 }
 
 const close = async () => {
-	if(!client) return;
-	
-	await client.close();
-	client = null;
-	database = null;
-	collection = null;
+	// close json server
 }
 
 module.exports = { 
@@ -64,6 +96,7 @@ module.exports = {
 	allCities,
 	removeCity,
 	updateCity,
+	updateCities,
 	clearDatabase,
 	connect,
 	close
